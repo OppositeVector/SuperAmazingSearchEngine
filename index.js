39ea13c4c84f26b36c69dcdc46d82067131f1b28
OppositeVector <iloriginal@gmil.com>
2015-10-21 18:53:04 +0300

    base Uplaod form

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var mongoose = require("mongoose");
 var url = require("url");
+var path = require("path");
 
 var port = process.env.PORT || 8080;
 
@@ -9,6 +10,8 @@ var app = express();
 app.get("/", function(req, res) {
 	res.send("Working !");
 });
+
+app.use("/upload", express.static('./public'));
 
 app.listen(port);
 console.log("listen on port " + port);
